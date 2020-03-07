22012f903aa2506aeb9b1abe34bb42b5794126dc
Qi Cao <alanqicao@gmail.com>
2020-03-07 14:21:29 -0700

    nothing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+  # def index
+  # end
 
-  def new
-  end
+  # def show
+  # end
+
+  # def new
+  # end
 
   def edit
+    @user 
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(user_params)
+  end
+  
 end
