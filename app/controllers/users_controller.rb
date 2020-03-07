d1ecebb347e4b66823dfad00d59d7086f96f38ee
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-07 12:16:37 -0700

    added user_name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,38 @@
 class UsersController < ApplicationController
-  def index
-  end
+  before_action :set_user, only: [:edit, :update, :destroy]
+  
+  # def index
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
   def edit
+    #Before Action
   end
+
+  def update
+    #Before Action
+    if user.update(user_params)
+      redirect_to movie_path
+    end
+  end
+
+  def destroy
+    #Before Action
+  end
+
+  private
+
+  def set_user
+    @user = User.find(user_params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
 end
