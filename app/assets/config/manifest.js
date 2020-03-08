7dbe085042af96e88e1ad0d9eb70c6fa24402e09
Qi Cao <alanqicao@gmail.com>
2020-03-08 00:55:38 -0700

    add JS

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+const sortByName_div = document.getElementById("sortByName")
+
+sortByName_div.addEventListener("click", function(){ alert("Sort By Name!") });
+
